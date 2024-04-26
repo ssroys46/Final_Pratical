a5f62241caff1c370b4e6b8c983393a87d04e387
ssroys46 <157700675+ssroys46@users.noreply.github.com>
2024-04-26 12:22:09 +0600

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,29 +1,62 @@
-function connect () {
-    var searchTerm = document.getElementById("searchBox").value ;
-    document.getElementById("searchBox").value = "" ; 
-                    var url = `https://www.themealdb.com/api/json/v1/1/search.php?s=${searchTerm}`; 
- 
-     fetch (url)
-     .then (res=> res.json() )
-     .then (data => display(data.meals) );
- }
- 
- 
- function display (items){
- 
-     var oldContent = document.getElementById("container");
-     oldContent.textContent = "";
-     for (var i=1; i<items.length; i++){
-      
-      var newDiv = document.createElement("div");
-      newDiv.innerHTML = `Meal id: <b> ${items[i-1].idMeal}</b><br>
-                          Meal Title: <b> ${items[i-1].strMeal}</b> <br>
-                          <img src="${items[i-1].strMealThumb}"> <br>
-                          Cooking Instructions: ${items[i-1].strInstructions} <br> <br>`;
-      newDiv.classList.add("innerStyle");
- 
-       oldContent.appendChild(newDiv);
- 
-     }
- 
- }
+function connect() {
+    var searchTerm = document.getElementById("searchBox").value;
+    var url = `https://www.themealdb.com/api/json/v1/1/search.php?s=${searchTerm}`;
+
+    fetch(url)
+        .then(res => res.json())
+        .then(data => {
+            display(data.meals);
+            if (data.meals) {
+                addSeeMoreButton();
+            }
+        });
+}
+
+function display(items) {
+    var container = document.getElementById("container");
+    container.textContent = "";
+    for (var i = 0; i < 5; i++) {
+        var newDiv = document.createElement("div");
+        newDiv.innerHTML = `Meal id: <b>${items[i].idMeal}</b><br>s
+                            Meal Title: <b>${items[i].strMeal}</b> <br>
+                            <img src="${items[i].strMealThumb}"> <br>
+                            Cooking Instructions: ${items[i].strInstructions} <br> <br>`;
+        newDiv.classList.add("innerStyle");
+        container.appendChild(newDiv);
+    }
+}
+
+function addSeeMoreButton() {
+    var container = document.getElementById("container");
+    var seeMoreButton = document.createElement("button");
+    seeMoreButton.textContent = "See More";
+    seeMoreButton.addEventListener('click', function() {
+        showMore();
+    });
+    container.appendChild(seeMoreButton);
+}
+
+function showMore() {
+    var searchTerm = document.getElementById("searchBox").value;
+    var url = `https://www.themealdb.com/api/json/v1/1/search.php?s=${searchTerm}`;
+
+    fetch(url)
+        .then(res => res.json())
+        .then(data => {
+            displayAll(data.meals);
+        });
+}
+
+function displayAll(items) {
+    var container = document.getElementById("container");
+    container.textContent = "";
+    for (var i = 0; i < items.length; i++) {
+        var newDiv = document.createElement("div");
+        newDiv.innerHTML = `Meal id: <b>${items[i].idMeal}</b><br>
+                            Meal Title: <b>${items[i].strMeal}</b> <br>
+                            <img src="${items[i].strMealThumb}"> <br>
+                            Cooking Instructions: ${items[i].strInstructions} <br> <br>`;
+        newDiv.classList.add("innerStyle");
+        container.appendChild(newDiv);
+    }
+}
